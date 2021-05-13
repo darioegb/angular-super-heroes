9801b2940ef84e2750356d789749edbf35dd69ef
Dario Gonzalez Bano <Dario.GonzalezBano@thomsonreuters.com>
2021-05-13 15:27:50 -0300

    fix problems with packages ungrade angular version

--- src/app/modules/super-hero/super-hero-grid/super-hero-grid.component.spec.ts
+++ src/app/modules/super-hero/super-hero-grid/super-hero-grid.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
@@ -10,7 +10,7 @@ describe('SuperHeroGridComponent', () => {
   let component: SuperHeroGridComponent;
   let fixture: ComponentFixture<SuperHeroGridComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SuperHeroGridComponent ],
       imports: [
