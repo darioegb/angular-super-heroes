b21eee8adf59da0f0dcaf27e942cd8121ffd99f3
Dario Gonzalez Banó <dario.gonzalezbano@naranja.com>
2021-09-24 12:01:55 -0300

    perform generic service

--- src/app/constants/globals.ts
+++ src/app/constants/globals.ts
@@ -2,6 +2,8 @@ export const assetRoot = 'assets';
 export const imgSrc = `${assetRoot}/imgs`;
 export const idKey = 'id';
 export const httpMethodKeys = {
+  get: 'get',
+  delete: 'delete',
   post: 'add',
   put: 'update',
 };
--- src/app/core/services/generic.service.ts
+++ src/app/core/services/generic.service.ts
@@ -9,6 +9,7 @@ import { map, catchError } from 'rxjs/operators';
 import { PageConfig, Page } from '@shared/models';
 import { ToastrService } from 'ngx-toastr';
 import { TranslateService } from '@ngx-translate/core';
+import { httpMethodKeys } from '@root/app/constants';
 
 export abstract class GenericService<T> {
   public readonly baseUrl = `${environment.apiUrl}/${this.getResourceUrl()}`;
@@ -64,30 +65,34 @@ export abstract class GenericService<T> {
   get(id: string | number): Observable<T> {
     return this.httpClient.get<T>(`${this.baseUrl}/${id}`).pipe(
       map((json) => this.fromServerModel(json)),
-      catchError((error) => this.handleError(error, 'get')),
+      catchError((error) => this.handleError(error, httpMethodKeys.get)),
     );
   }
 
   add(resource: T): Observable<unknown> {
     return this.httpClient
       .post(`${this.baseUrl}`, this.toServerModel(resource))
-      .pipe(catchError((error) => this.handleError(error, 'add')));
+      .pipe(
+        catchError((error) => this.handleError(error, httpMethodKeys.post)),
+      );
   }
 
   delete(id: string | number): Observable<unknown> {
     return this.httpClient
       .delete(`${this.baseUrl}/${id}`)
-      .pipe(catchError((error) => this.handleError(error, 'delete')));
+      .pipe(
+        catchError((error) => this.handleError(error, httpMethodKeys.delete)),
+      );
   }
 
-  update(resource: T): Observable<Object> {
+  update(resource: T): Observable<unknown> {
     const resourceServer = this.toServerModel(resource);
     return this.httpClient
       .put(`${this.baseUrl}/${resourceServer.id}`, resourceServer)
-      .pipe(catchError((error) => this.handleError(error, 'update')));
+      .pipe(catchError((error) => this.handleError(error, httpMethodKeys.put)));
   }
 
-  private handleError(
+  protected handleError(
     error: HttpErrorResponse,
     operation: string,
   ): Observable<never> {
